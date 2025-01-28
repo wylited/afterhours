150921e2565f05f9e941cc1724be129addfc78a3
Alex <climie.alex@gmail.com>
2025-01-28 13:43:18 +0800

    working light

--- src/demo/mask.rs
+++ src/demo/mask.rs
@@ -7,13 +7,15 @@ use bevy::{
 use bevy_light_2d::prelude::*;
 
 use crate::AppSet;
+use super::player::Player; 
+
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(Light2dPlugin)
         .add_systems(
             Update,
         
-            toggle_mask.in_set(AppSet::RecordInput),
+            (toggle_mask.in_set(AppSet::RecordInput), update_light_position),
         );
 }
 
@@ -44,10 +46,6 @@ fn spawn_mask(
         commands.entity(camera_entity).despawn();
     }
 
-
-
- 
-
     let mut projection = OrthographicProjection::default_2d();
     projection.scale = 1.;
 
@@ -61,18 +59,42 @@ fn spawn_mask(
         IsDefaultUiCamera
     ));
 
-    commands.spawn(PointLight2d {
-        intensity: 3.0,
-        radius: 100.0,
+    commands.spawn((
+        Mask {state:true},
+        Transform::from_xyz(0.0, 4.0, 1.),
+        PointLight2d {
+        intensity: 2.0,
+        radius: 200.0,
         ..default()
-    });
+    },
+   
+    ));
 }
 
 
-fn toggle_mask(input: Res<ButtonInput<KeyCode>>, mut mask_query: Query<&mut Mask>) {
+fn toggle_mask(input: Res<ButtonInput<KeyCode>>, mut mask_query: Query<(&mut Mask, &mut PointLight2d)>) {
     if input.just_pressed(KeyCode::Space) {
-        if let Ok(mut mask) = mask_query.get_single_mut() {
+        if let Ok((mut mask, mut PointLight2d)) = mask_query.get_single_mut() {
             mask.state = !mask.state;
+
+            if mask.state {
+                PointLight2d.intensity = 3.;
+            }
+            else {
+                PointLight2d.intensity = 0.;
+            }
+        }
+    }
+}
+
+
+fn update_light_position(
+    player_query: Query<&Transform, With<Player>>, // Query the player's position
+    mut light_query: Query<&mut Transform, (With<Mask>, Without<Player>)>, // Query the light's position
+) {
+    if let Ok(player_transform) = player_query.get_single() {
+        if let Ok(mut light_transform) = light_query.get_single_mut() {
+            light_transform.translation = player_transform.translation;
         }
     }
 }
